51aee962db584cda592db455c3acb8d1df30ce1f
Ceren Yıldırım <mrs.directioner01@hotmail.com>
2022-12-27 20:06:23 +0300

    updates

--- Grup13/Grup13/gorevlendirici/Process.java
+++ Grup13/Grup13/gorevlendirici/Process.java
@@ -5,13 +5,17 @@ public class Process {
 	int varisZamani;
 	int oncelik;
 	int processZamani;
-	boolean dahaOnceCalistiMi; //1 ise basladi,2 ise askida,3 ise yurutuluyor,4 sonlandi
+	boolean dahaOnceCalistiMi; // 1 ise basladi,2 ise askida,3 ise yurutuluyor,4 sonlandi
 
 	public Process(String pid, int varisZamani, int oncelik, int processZamani) {
 		this.pid = pid;
 		this.oncelik = oncelik;
 		this.processZamani = processZamani;
 		this.varisZamani = varisZamani;
+	}
+
+	public void Calistir() {
+		processZamani = processZamani - 1;
 	}
 
 }
